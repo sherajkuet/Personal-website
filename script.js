5af9eb80b1876547911ef4afab896c16f3f83c16
sherajkuet <69256651+sherajkuet@users.noreply.github.com>
2023-08-18 22:34:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// Add dynamic publications to the publications section
+const publications = [
+    "Publication 1",
+    "Publication 2",
+    "Publication 3"
+    // Add more publications as needed
+];
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+const publicationList = document.getElementById("publication-list");
+publications.forEach(publication => {
+    const publicationItem = document.createElement("li");
+    publicationItem.textContent = publication;
+    publicationList.appendChild(publicationItem);
+});
+
+// Handle contact form submission
+const contactForm = document.getElementById("contact-form");
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // You can implement sending the form data to a server or perform other actions here
+
+    // Clear the form fields after submission
+    contactForm.reset();
 });
