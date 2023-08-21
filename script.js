319ddd93784ca5784fd560a5349d3f625813cd7a
sherajkuet <69256651+sherajkuet@users.noreply.github.com>
2023-08-21 12:41:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// JavaScript function to show/hide sections
+function showSection(sectionId) {
+    // Hide all sections
+    var sections = document.querySelectorAll('.content');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].style.display = 'none';
+    }
+
+    // Show the selected section
+    var section = document.getElementById(sectionId);
+    if (section) {
+        section.style.display = 'block';
+    }
+}
+
+// Show the 'home' section by default
+showSection('home');
 
