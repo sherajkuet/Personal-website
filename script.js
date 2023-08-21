d7524a4a12cc3889a59cfadd6fbc321bb8be8ff2
sherajkuet <69256651+sherajkuet@users.noreply.github.com>
2023-08-21 01:10:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,13 @@
-// Add dynamic publications to the publications section
-const publications = [
-    "Publication 1",
-    "Publication 2",
-    "Publication 3"
-    // Add more publications as needed
-];
+// JavaScript to toggle the visibility of the navigation bar
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButton = document.getElementById("toggleNav");
+    const navbar = document.getElementById("navbar");
 
-const publicationList = document.getElementById("publication-list");
-publications.forEach(publication => {
-    const publicationItem = document.createElement("li");
-    publicationItem.textContent = publication;
-    publicationList.appendChild(publicationItem);
-});
-
-// Handle contact form submission
-const contactForm = document.getElementById("contact-form");
-
-contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    // You can implement sending the form data to a server or perform other actions here
-
-    // Clear the form fields after submission
-    contactForm.reset();
+    toggleButton.addEventListener("click", function () {
+        if (navbar.style.display === "block") {
+            navbar.style.display = "none";
+        } else {
+            navbar.style.display = "block";
+        }
+    });
 });
