72a47e75caac01671d45bf7321aa677ddb20f353
sherajkuet <69256651+sherajkuet@users.noreply.github.com>
2023-08-21 12:01:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// JavaScript to toggle the visibility of the navigation bar
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.getElementById("toggleNav");
-    const navbar = document.getElementById("navbar");
 
-    toggleButton.addEventListener("click", function () {
-        if (navbar.style.display === "block") {
-            navbar.style.display = "none";
-        } else {
-            navbar.style.display = "block";
-        }
-    });
-});
