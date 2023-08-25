6328bf1a5eb01db57530712edfc2d951774ae37f
sherajkuet <69256651+sherajkuet@users.noreply.github.com>
2023-08-25 19:37:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,25 @@ function showSection(sectionId) {
     if (section) {
         section.style.display = 'block';
     }
+
+    // Increment visitor count
+    incrementVisitorCount();
 }
 
 // Show the 'home' section by default
 showSection('home');
+
+// JavaScript function to increment the visitor count
+function incrementVisitorCount() {
+    var visitorCountElement = document.getElementById('visitor-count');
+    if (visitorCountElement) {
+        var currentCount = parseInt(visitorCountElement.innerText.split(': ')[1]);
+        if (!isNaN(currentCount)) {
+            visitorCountElement.innerText = 'Visitors: ' + (currentCount + 1);
+        } else {
+            visitorCountElement.innerText = 'Visitors: 1'; // Set initial count if NaN
+        }
+    }
+}
+
 
